e91e9562af26d6845b6d3a8b9e34a4279f9f70eb
jianhua <54907@DESKTOP-J9CIO3P>
2019-01-08 16:23:17 +0800

    第二个提交

--- src/Test2.java
+++ src/Test2.java
@@ -1,4 +1,7 @@
 public class Test2 {
+    public void firstAlter(){
+        System.out.println("a");
+    }
     public static void main(String[] args){
         System.out.println("ccccccccccccccc");
     }
